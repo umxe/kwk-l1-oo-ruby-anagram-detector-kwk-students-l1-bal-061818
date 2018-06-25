82a0915b12c471429ef570db6f8f29085578f8b4
umxe <u_habiba@rocketmail.com>
2018-06-25 14:21:48 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,33 @@
 # Your code goes here!
+class Anagram
+  arrE = []
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+  anagram_groups = []
+  words.each do |word|
+
+    word_added = false
+
+    anagram_groups.each do |group|
+      if group.include? word
+        word_added = true
+      elsif word.downcase.chars.sort == group[0].downcase.chars.sort
+        group << word
+        word_added = true        
+      end
+    end
+
+    unless word_added
+      anagram_groups << [word]
+      word_added = true
+    end
+
+  end
+  return anagram_groups
+end
+
+  end
+end
